0f1c9a057928ac0e1dc56d0312f596514075fa47
p00149021@houstonisd.org Mag17615@7 <lioneljones5116@gmail.com>
2022-03-08 09:50:22 -0600

    Minor Changes

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,7 @@
 let Config = {};
 
 //Config.REST_URL = 'http://appsdev.houstonisd.org/HISDDevAppsWEBAPI/';
+//Config.REST_URL = 'http://appsdev.houstonisd.org/SBHISDCustomDevAppsWebAPI';
 Config.REST_URL = 'http://localhost/HISDCustAppsWebAPI';
 //Config.REST_URL = 'http://localhost:54751';
 Config.REST_URL_AD_AUTH = 'https://apiint.houstonisd.org/common/api/userinfo';
